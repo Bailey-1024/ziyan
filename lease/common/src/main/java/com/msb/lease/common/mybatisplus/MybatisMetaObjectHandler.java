10531e8fde9dfa3cbbdd7c4f4f03b350c29195b9
fruit <2759711431@qq.com>
2024-07-03 18:31:42 +0800

    ???

---
+++ lease/common/src/main/java/com/msb/lease/common/mybatisplus/MybatisMetaObjectHandler.java
@@ -0,0 +1,23 @@
+package com.msb.lease.common.mybatisplus;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * 配置mybatis-plus在自动填充字段内容时候的规则
+ */
+@Component
+public class MybatisMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject, "createTime", Date.class, new Date());
+        this.strictInsertFill(metaObject, "isDeleted", Byte.class, (byte) 0);
+    }
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject, "updateTime", Date.class, new Date());
+    }
+}
