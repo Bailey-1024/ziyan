8ee0def716336e07903ebdcd06f81790733646b3
fruit <2759711431@qq.com>
2024-07-04 21:31:07 +0800

    完善公寓管理模块5

---
+++ lease/common/src/main/java/com/msb/lease/common/exception/GlobalExcetptionHandler.java
@@ -0,0 +1,24 @@
+package com.msb.lease.common.exception;
+
+import com.msb.lease.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 添加全局异常处理类
+ */
+@ControllerAdvice
+public class GlobalExcetptionHandler {
+    /**
+     * 统一捕获所有的异常
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseBody
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+}
--- lease/web/web-admin/src/main/java/com/msb/lease/web/admin/service/impl/FileServiceImpl.java
+++ lease/web/web-admin/src/main/java/com/msb/lease/web/admin/service/impl/FileServiceImpl.java
@@ -2,18 +2,21 @@ package com.msb.lease.web.admin.service.impl;
 
 import com.msb.lease.common.minio.MinioProperties;
 import com.msb.lease.web.admin.service.FileService;
-import io.minio.BucketExistsArgs;
-import io.minio.MakeBucketArgs;
-import io.minio.MinioClient;
-import io.minio.SetBucketPolicyArgs;
+import io.minio.*;
 import io.minio.errors.*;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.attribute.standard.DateTimeAtCompleted;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+import java.util.logging.SimpleFormatter;
 
 @Service
 public class FileServiceImpl implements FileService {
@@ -27,9 +30,9 @@ public class FileServiceImpl implements FileService {
      * @return
      */
     @Override
-    public String upload(MultipartFile file) {
-        //判断是否存在对应的文件夹
-        try {
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+
+            //判断是否存在对应的文件夹
             boolean bucketExiste = minioClient.bucketExists(
                     BucketExistsArgs
                             .builder()
@@ -49,15 +52,40 @@ public class FileServiceImpl implements FileService {
                 minioClient.setBucketPolicy(
                         SetBucketPolicyArgs
                                 .builder()
-                                .bucket(minioProperties.getBucketName())
+                                .bucket(minioProperties.getBucketName())//给哪个桶修改访问权限
                                 .config(createBucketPolicyConfig(minioProperties.getBucketName()))
                                 .build()
                 );
             }
-        } catch (Exception e) {
-           e.printStackTrace();
-        }
-        return null;
+            String filename = new SimpleDateFormat("yyyyMMdd")
+                    .format(new Date())
+                    +"/"+
+                    UUID.randomUUID()
+                    +"-"+
+                    file.getOriginalFilename();
+
+            //将本地文件上传到minIO服务器
+            minioClient.putObject(
+                    PutObjectArgs
+                            .builder()
+                            //将数据保存到哪个文件夹中
+                            .bucket(minioProperties.getBucketName())
+                            //保存的文件名称
+                            .object(filename)
+                            //以流的形式，将文件上传到服务器中
+                            .stream(file.getInputStream(),file.getSize(),-1)
+                            //设置请求的类型
+                            .contentType(file.getContentType())
+                            .build()
+            );
+            //返回文件存储路径
+            return String.join(
+                    "/",
+                    minioProperties.getEndpoint(),
+                    minioProperties.getBucketName(),
+                    filename);
+
+
     }
 
     /**
@@ -66,6 +94,11 @@ public class FileServiceImpl implements FileService {
      * @return
      */
     private String createBucketPolicyConfig(String bucketName) {
+        /**
+         * 自定义访问规则
+         *  允许所有人访问
+         *  只允许自己写
+         */
         return """
                 {
                     "Version": "2012-10-17",
