bc3c49feb35b3155c077c531e2c7b66a99a0ef07
””Bailey“ <784280159@qq.com>
2024-07-04 21:25:14 +0800

    租约分页查询

--- lease/web/web-admin/src/main/java/com/msb/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
+++ lease/web/web-admin/src/main/java/com/msb/lease/web/admin/service/impl/LeaseAgreementServiceImpl.java
@@ -1,9 +1,13 @@
 package com.msb.lease.web.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.msb.lease.model.entity.LeaseAgreement;
 import com.msb.lease.web.admin.mapper.LeaseAgreementMapper;
 import com.msb.lease.web.admin.service.LeaseAgreementService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.msb.lease.web.admin.vo.agreement.AgreementQueryVo;
+import com.msb.lease.web.admin.vo.agreement.AgreementVo;
+import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,7 +18,13 @@ import org.springframework.stereotype.Service;
 @Service
 public class LeaseAgreementServiceImpl extends ServiceImpl<LeaseAgreementMapper, LeaseAgreement>
         implements LeaseAgreementService {
-
+    @Resource
+    private LeaseAgreementMapper mapper;
+    //分页查询
+    @Override
+    public IPage<AgreementVo> selectLAPage(IPage<AgreementVo> page, AgreementQueryVo queryVo) {
+        return mapper.selectLAPage(page,queryVo);
+    }
 }
 
 
