bee36b597758cde050004133192563922d0681f6
fruit <2759711431@qq.com>
2024-07-04 08:15:55 +0800

    添加配置类

--- lease/web/web-admin/src/main/java/com/msb/lease/web/admin/controller/apartment/LabelController.java
+++ lease/web/web-admin/src/main/java/com/msb/lease/web/admin/controller/apartment/LabelController.java
@@ -1,11 +1,15 @@
 package com.msb.lease.web.admin.controller.apartment;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.msb.lease.common.result.Result;
 import com.msb.lease.model.entity.LabelInfo;
 import com.msb.lease.model.enums.ItemType;
+import com.msb.lease.web.admin.service.LabelInfoService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +18,16 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/label")
 public class LabelController {
+    @Resource
+    private LabelInfoService labelInfoService;
 
     @Operation(summary = "（根据类型）查询标签列表")
     @GetMapping("list")
     public Result<List<LabelInfo>> labelList(@RequestParam(required = false) ItemType type) {
-        return Result.ok();
+        LambdaQueryWrapper<LabelInfo> labelInfoQueryWrapper = new LambdaQueryWrapper<>();
+        labelInfoQueryWrapper.eq(type!=null,LabelInfo::getType,type);
+        List<LabelInfo> list = labelInfoService.list(labelInfoQueryWrapper);
+        return Result.ok(list);
     }
 
     @Operation(summary = "新增或修改标签信息")
