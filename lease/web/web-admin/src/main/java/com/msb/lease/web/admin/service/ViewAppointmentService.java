c552a3d76c9a49f29b7dac8a66f12fbfc669db6b
””Bailey“ <784280159@qq.com>
2024-07-04 10:27:15 +0800

    123456

--- lease/web/web-admin/src/main/java/com/msb/lease/web/admin/service/ViewAppointmentService.java
+++ lease/web/web-admin/src/main/java/com/msb/lease/web/admin/service/ViewAppointmentService.java
@@ -13,4 +13,6 @@ import com.baomidou.mybatisplus.extension.service.IService;
 */
 public interface ViewAppointmentService extends IService<ViewAppointment> {
 
+
+    IPage<AppointmentVo> selectAVPage(IPage<AppointmentVo> page, AppointmentQueryVo queryVo);
 }
