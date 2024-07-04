f0488da0f660f785beef259350dca0c001d1c4cf
2522399067 <2522399067@qq.com>
2024-07-04 11:38:58 +0800

    mzc

--- lease/web/web-admin/src/main/java/com/msb/lease/AdminWebApplication.java
+++ lease/web/web-admin/src/main/java/com/msb/lease/AdminWebApplication.java
@@ -1,9 +1,13 @@
 package com.msb.lease;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+
+@MapperScan("com.msb.lease.web.admin.mapper")
 public class AdminWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminWebApplication.class,args);
