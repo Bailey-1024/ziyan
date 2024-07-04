48d40791ce1866714df8a6491bf370c8e5b4c5fa
root <qq1191923415@gmail.com>
2024-07-04 16:11:36 +0800

    redis配置

--- lease/web/web-admin/src/main/java/com/msb/lease/web/admin/controller/login/LoginController.java
+++ lease/web/web-admin/src/main/java/com/msb/lease/web/admin/controller/login/LoginController.java
@@ -2,17 +2,22 @@ package com.msb.lease.web.admin.controller.login;
 
 
 import com.msb.lease.common.result.Result;
+import com.msb.lease.web.admin.service.LoginService;
 import com.msb.lease.web.admin.vo.login.CaptchaVo;
 import com.msb.lease.web.admin.vo.login.LoginVo;
 import com.msb.lease.web.admin.vo.system.user.SystemUserInfoVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "后台管理系统登录管理")
 @RestController
 @RequestMapping("/admin")
 public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
 
     @Operation(summary = "获取图形验证码")
     @GetMapping("login/captcha")
