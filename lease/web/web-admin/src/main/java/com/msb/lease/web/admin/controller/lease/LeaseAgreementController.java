ab9774125502f7105851ef16f929fd7d7bc40698
””Bailey“ <784280159@qq.com>
2024-07-05 10:33:32 +0800

    模块完成

--- lease/web/web-admin/src/main/java/com/msb/lease/web/admin/controller/lease/LeaseAgreementController.java
+++ lease/web/web-admin/src/main/java/com/msb/lease/web/admin/controller/lease/LeaseAgreementController.java
@@ -1,6 +1,7 @@
 package com.msb.lease.web.admin.controller.lease;
 
 
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.msb.lease.common.result.Result;
 import com.msb.lease.model.entity.LeaseAgreement;
@@ -25,6 +26,7 @@ public class LeaseAgreementController {
     @Operation(summary = "保存或修改租约信息")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdate(@RequestBody LeaseAgreement leaseAgreement) {
+        service.saveOrUpdate(leaseAgreement);
         return Result.ok();
     }
 
@@ -42,18 +44,23 @@ public class LeaseAgreementController {
     @Operation(summary = "根据id查询租约信息")
     @GetMapping(name = "getById")
     public Result<AgreementVo> getById(@RequestParam Long id) {
-        return Result.ok();
+        AgreementVo result=service.getLessAgreementById(id);
+        return Result.ok(result);
     }
 
     @Operation(summary = "根据id删除租约信息")
     @DeleteMapping("removeById")
     public Result removeById(@RequestParam Long id) {
+        service.removeById(id);
         return Result.ok();
     }
 
     @Operation(summary = "根据id更新租约状态")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam LeaseStatus status) {
+        LambdaUpdateWrapper<LeaseAgreement> updateWrapper = new LambdaUpdateWrapper<>();
+        updateWrapper.eq(LeaseAgreement::getId,id).set(LeaseAgreement::getStatus,status);
+        service.update(updateWrapper);
         return Result.ok();
     }
 
